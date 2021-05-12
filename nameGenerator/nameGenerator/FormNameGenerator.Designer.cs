184ac02d1ec2e75613fec134dbba47bb2b19efcb
Enes Cerrahoğlu <enescerrahoglu1@gmail.com>
2021-05-13 00:13:29 +0300

    Add files via upload

---
+++ nameGenerator/nameGenerator/FormNameGenerator.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace nameGenerator
+{
+    partial class FormNameGenerator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_generate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox_name = new System.Windows.Forms.TextBox();
+            this.lbl_count = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.toggleDark = new nameGenerator.Toggle();
+            this.radioButton_lCase = new System.Windows.Forms.RadioButton();
+            this.radioButton_uCase = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_generate
+            // 
+            this.button_generate.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button_generate.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button_generate.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button_generate.Location = new System.Drawing.Point(12, 159);
+            this.button_generate.Name = "button_generate";
+            this.button_generate.Size = new System.Drawing.Size(300, 40);
+            this.button_generate.TabIndex = 0;
+            this.button_generate.Text = "İsim Üret";
+            this.button_generate.UseVisualStyleBackColor = false;
+            this.button_generate.Click += new System.EventHandler(this.button_generate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-277, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-334, -55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textbox_name
+            // 
+            this.textbox_name.BackColor = System.Drawing.Color.AliceBlue;
+            this.textbox_name.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textbox_name.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.textbox_name.Location = new System.Drawing.Point(12, 126);
+            this.textbox_name.Name = "textbox_name";
+            this.textbox_name.Size = new System.Drawing.Size(300, 27);
+            this.textbox_name.TabIndex = 4;
+            // 
+            // lbl_count
+            // 
+            this.lbl_count.AutoSize = true;
+            this.lbl_count.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_count.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbl_count.Location = new System.Drawing.Point(12, 12);
+            this.lbl_count.Name = "lbl_count";
+            this.lbl_count.Size = new System.Drawing.Size(120, 19);
+            this.lbl_count.TabIndex = 5;
+            this.lbl_count.Text = "Deneme sayısı: 0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.AliceBlue;
+            this.numericUpDown1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.numericUpDown1.Location = new System.Drawing.Point(119, 95);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 23);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Tag = "";
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "bababa....",
+            "ababab....",
+            "abbab.....",
+            "babbab"});
+            this.comboBox1.Location = new System.Drawing.Point(119, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(193, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbl2.Location = new System.Drawing.Point(12, 95);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(96, 23);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "Harf Sayısı:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbl1.Location = new System.Drawing.Point(12, 66);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(101, 23);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "Format Seç:";
+            // 
+            // toggleDark
+            // 
+            this.toggleDark.BorderColor = System.Drawing.Color.AliceBlue;
+            this.toggleDark.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.toggleDark.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.toggleDark.ForeColor = System.Drawing.Color.White;
+            this.toggleDark.IsOn = false;
+            this.toggleDark.Location = new System.Drawing.Point(266, 12);
+            this.toggleDark.Name = "toggleDark";
+            this.toggleDark.OffColor = System.Drawing.Color.DarkGray;
+            this.toggleDark.OffText = "OFF";
+            this.toggleDark.OnColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.toggleDark.OnText = "ON";
+            this.toggleDark.Size = new System.Drawing.Size(46, 25);
+            this.toggleDark.TabIndex = 10;
+            this.toggleDark.TextEnabled = true;
+            this.toggleDark.Click += new System.EventHandler(this.ceLearningToggle1_Click);
+            // 
+            // radioButton_lCase
+            // 
+            this.radioButton_lCase.AutoSize = true;
+            this.radioButton_lCase.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton_lCase.Location = new System.Drawing.Point(191, 95);
+            this.radioButton_lCase.Name = "radioButton_lCase";
+            this.radioButton_lCase.Size = new System.Drawing.Size(56, 19);
+            this.radioButton_lCase.TabIndex = 12;
+            this.radioButton_lCase.TabStop = true;
+            this.radioButton_lCase.Text = "LCase";
+            this.radioButton_lCase.UseVisualStyleBackColor = true;
+            this.radioButton_lCase.CheckedChanged += new System.EventHandler(this.radioButton_lCase_CheckedChanged);
+            // 
+            // radioButton_uCase
+            // 
+            this.radioButton_uCase.AutoSize = true;
+            this.radioButton_uCase.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton_uCase.Location = new System.Drawing.Point(253, 95);
+            this.radioButton_uCase.Name = "radioButton_uCase";
+            this.radioButton_uCase.Size = new System.Drawing.Size(59, 19);
+            this.radioButton_uCase.TabIndex = 13;
+            this.radioButton_uCase.TabStop = true;
+            this.radioButton_uCase.Text = "UCase";
+            this.radioButton_uCase.UseVisualStyleBackColor = true;
+            this.radioButton_uCase.CheckedChanged += new System.EventHandler(this.radioButton_uCase_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(324, 208);
+            this.Controls.Add(this.radioButton_uCase);
+            this.Controls.Add(this.radioButton_lCase);
+            this.Controls.Add(this.toggleDark);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lbl_count);
+            this.Controls.Add(this.textbox_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_generate);
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Name Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_generate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textbox_name;
+        private System.Windows.Forms.Label lbl_count;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private Toggle toggleDark;
+        private System.Windows.Forms.RadioButton radioButton_lCase;
+        private System.Windows.Forms.RadioButton radioButton_uCase;
+    }
+}
+
---
+++ nameGenerator/nameGenerator/FormNameGenerator.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToggleButtonExample;
+
+namespace nameGenerator
+{
+    public partial class FormNameGenerator : Form
+    {
+        public FormNameGenerator()
+        {
+            using (FormWaiting waiting = new FormWaiting(Wait))
+            {
+                waiting.ShowDialog(this);
+            }
+            CenterToScreen();
+            InitializeComponent();
+            Toggle toggleSwitch = new Toggle();
+        }
+
+        void Wait()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private static int i;
+        private void button_generate_Click(object sender, EventArgs e)
+        {
+            i++;
+            this.lbl_count.Text = "Deneme sayısı: " + i.ToString();
+            if (comboBox1.SelectedIndex == 0)
+            {
+                this.textbox_name.Text = generateName(Convert.ToInt32(numericUpDown1.Value));
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                this.textbox_name.Text = generateName2(Convert.ToInt32(numericUpDown1.Value));
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                this.textbox_name.Text = generateName3(Convert.ToInt32(numericUpDown1.Value));
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                this.textbox_name.Text = generateName4(Convert.ToInt32(numericUpDown1.Value));
+                if (numericUpDown1.Value > 6)
+                {
+                    this.textbox_name.Text = textbox_name.Text.Remove(6);
+                }
+            }
+            else
+            {
+                lbl_count.Text = "Deneme sayısı: 0";
+                i = 0;
+                this.textbox_name.Text = "Lütfen önce istediğiniz bir formatı seçin!";
+            }
+
+            if (this.textbox_name.Text == "enes")
+            {
+                button_generate.Enabled = false;
+            }
+        }
+
+        public static string generateName(int length)
+        {
+            Random random = new Random();
+            string[] quiet = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] loud = { "a", "e", "i", "o", "u" };
+            string quiteLetter;
+            string loudLetter;
+            //int nameLength = 5;
+            int i = 0;
+            string name = "";
+            while (i < length)
+            {
+                i += 1;
+                int l1 = random.Next(quiet.Length);
+                quiteLetter = (string)quiet[l1];
+                name = name + quiteLetter;
+                if (i == length)
+                {
+                    break;
+                }
+                else
+                {
+                    i += 1;
+                    int l2 = random.Next(loud.Length);
+                    loudLetter = (string)loud[l2];
+                    name = name + loudLetter;
+                    if (i == length)
+                    {
+                        break;
+                    }
+                }
+            }
+            return name;
+        }
+
+        public static string generateName2(int length)
+        {
+            Random random = new Random();
+            string[] quiet = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] loud = { "a", "e", "i", "o", "u" };
+            string quiteLetter;
+            string loudLetter;
+            //int nameLength = 5;
+            int i = 0;
+            string name = "";
+            while (i < length)
+            {
+                i += 1;
+                int l2 = random.Next(loud.Length);
+                loudLetter = (string)loud[l2];
+                name = name + loudLetter;
+                if (i == length)
+                {
+                    break;
+                }
+                else
+                {
+                    i += 1;
+                    int l1 = random.Next(quiet.Length);
+                    quiteLetter = (string)quiet[l1];
+                    name = name + quiteLetter;
+                    if (i == length)
+                    {
+                        break;
+                    }
+                }
+            }
+            return name;
+        }
+
+        public static string generateName3(int length)
+        {
+            Random random = new Random();
+            string[] quiet = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] loud = { "a", "e", "i", "o", "u" };
+            string quiteLetter;
+            string loudLetter;
+            //int nameLength = 5;
+            int i = 0;
+            string name = "";
+            while (i < length)
+            {
+                i += 1;
+                int l1 = random.Next(loud.Length);
+                loudLetter = (string)loud[l1];
+                name = name + loudLetter;
+                if (i == length)
+                {
+                    break;
+                }
+                else
+                {
+                    i += 1;
+                    int l2 = random.Next(quiet.Length);
+                    quiteLetter = (string)quiet[l2];
+                    name = name + quiteLetter;
+                    if (i == length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        i += 1;
+                        int l3 = random.Next(quiet.Length);
+                        quiteLetter = (string)quiet[l3];
+                        name = name + quiteLetter;
+                        if (i == length)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            i += 1;
+                            int l4 = random.Next(loud.Length);
+                            loudLetter = (string)loud[l4];
+                            name = name + loudLetter;
+                            if (i == length)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                i += 1;
+                                int l5 = random.Next(quiet.Length);
+                                quiteLetter = (string)quiet[l5];
+                                name = name + quiteLetter;
+                                if (i == length)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return name;
+        }
+
+        public static string generateName4(int length)
+        {
+            Random random = new Random();
+            string[] quiet = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+            string[] loud = { "a", "e", "i", "o", "u" };
+            string quiteLetter;
+            string loudLetter;
+            //int nameLength = 5;
+            int i = 0;
+            string name = "";
+            while (i < length)
+            {
+                i += 1;
+                int l1 = random.Next(quiet.Length);
+                quiteLetter = (string)quiet[l1];
+                name = name + quiteLetter;
+                if (i == length)
+                {
+                    break;
+                }
+                else
+                {
+                    i += 1;
+                    int l2 = random.Next(loud.Length);
+                    loudLetter = (string)loud[l2];
+                    name = name + loudLetter;
+                    if (i == length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        i += 1;
+                        int l3 = random.Next(quiet.Length);
+                        quiteLetter = (string)quiet[l3];
+                        name = name + quiteLetter;
+                        if (i == length)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            i += 1;
+                            int l4 = random.Next(quiet.Length);
+                            quiteLetter = (string)quiet[l4];
+                            name = name + quiteLetter;
+                            if (i == length)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                i += 1;
+                                int l5 = random.Next(loud.Length);
+                                loudLetter = (string)loud[l5];
+                                name = name + loudLetter;
+                                if (i == length)
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    i += 1;
+                                    int l6 = random.Next(quiet.Length);
+                                    quiteLetter = (string)quiet[l6];
+                                    name = name + quiteLetter;
+                                    if (i == length)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return name;
+        }
+
+        private void ceLearningToggle1_Click(object sender, EventArgs e)
+        {
+            if (toggleDark.IsOn == true)
+            {
+                this.BackColor = System.Drawing.Color.MidnightBlue;
+                toggleDark.BorderColor = System.Drawing.Color.MidnightBlue;
+                button_generate.ForeColor = System.Drawing.Color.MidnightBlue;
+                button_generate.BackColor = System.Drawing.Color.AliceBlue;
+                lbl_count.ForeColor = System.Drawing.Color.AliceBlue;
+                lbl1.ForeColor = System.Drawing.Color.AliceBlue;
+                lbl2.ForeColor = System.Drawing.Color.AliceBlue;
+                comboBox1.ForeColor = System.Drawing.Color.MidnightBlue;
+                comboBox1.BackColor = System.Drawing.Color.AliceBlue;
+                numericUpDown1.ForeColor = System.Drawing.Color.AliceBlue;
+                numericUpDown1.BackColor = System.Drawing.Color.MidnightBlue;
+                textbox_name.ForeColor = System.Drawing.Color.AliceBlue;
+                textbox_name.BackColor = System.Drawing.Color.MidnightBlue;
+                radioButton_uCase.ForeColor = System.Drawing.Color.AliceBlue;
+                radioButton_lCase.ForeColor = System.Drawing.Color.AliceBlue;
+            }
+            else if (toggleDark.IsOn == false)
+            {
+                this.BackColor = System.Drawing.Color.AliceBlue;
+                toggleDark.BorderColor = System.Drawing.Color.AliceBlue;
+                button_generate.ForeColor = System.Drawing.Color.AliceBlue;
+                button_generate.BackColor = System.Drawing.Color.MidnightBlue;
+                lbl_count.ForeColor = System.Drawing.Color.MidnightBlue;
+                lbl1.ForeColor = System.Drawing.Color.MidnightBlue;
+                lbl2.ForeColor = System.Drawing.Color.MidnightBlue;
+                comboBox1.ForeColor = System.Drawing.Color.AliceBlue;
+                comboBox1.BackColor = System.Drawing.Color.MidnightBlue;
+                numericUpDown1.ForeColor = System.Drawing.Color.MidnightBlue;
+                numericUpDown1.BackColor = System.Drawing.Color.AliceBlue;
+                textbox_name.ForeColor = System.Drawing.Color.MidnightBlue;
+                textbox_name.BackColor = System.Drawing.Color.AliceBlue;
+                radioButton_uCase.ForeColor = System.Drawing.Color.MidnightBlue;
+                radioButton_lCase.ForeColor = System.Drawing.Color.MidnightBlue;
+
+            }
+        }
+
+        private void radioButton_lCase_CheckedChanged(object sender, EventArgs e)
+        {
+            textbox_name.CharacterCasing = CharacterCasing.Lower;
+        }
+
+        private void radioButton_uCase_CheckedChanged_1(object sender, EventArgs e)
+        {
+            textbox_name.CharacterCasing = CharacterCasing.Upper;
+        }
+    }
+}
